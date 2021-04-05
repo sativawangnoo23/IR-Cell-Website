3b9b50744f9a07c22c768c20ee7d267b287a7f37
Sativa Wangnoo <sativawangnoo23@gmail.com>
2021-04-05 12:45:46 +0530

    Static files linked

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"));
+
 app.get('/', function (req, res) {
   console.log("Homepage Opened");
   res.sendFile(__dirname+'/public/index.html')
