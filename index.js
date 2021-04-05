d94ec4b767c7896a95437bf9e8ec0af5565bbb36
Sativa Wangnoo <sativawangnoo23@gmail.com>
2021-04-05 12:46:43 +0530

    Contact html and JS created, linked

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"));
+
 app.get('/', function (req, res) {
   console.log("Homepage Opened");
   res.sendFile(__dirname+'/public/index.html')
+})
+
+app.get('/contact', function (req, res) {
+  console.log("Contact form Opened");
+  res.sendFile(__dirname+'/public/contact.html')
 })
 
 app.listen(3000,function() {
