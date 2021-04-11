d6095f5500880056886a05a9a1ec84dfaa027c2c
Sativa Wangnoo <sativawangnoo23@gmail.com>
2021-04-11 08:02:19 +0530

    User input accesed

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   console.log("Homepage Opened");
   res.sendFile(__dirname+'/public/index.html')
 })
 
+// Contact form
 app.get('/contact', function (req, res) {
   console.log("Contact form Opened");
   res.sendFile(__dirname+'/public/contact.html')
+})
+
+app.post('/contact', function(req,res) {
+  console.log(req.body)
 })
 
 app.listen(3000,function() {
