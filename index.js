34832ed524d950a23a0146ccfacdcb37a9ce4108
Sativa Wangnoo <sativawangnoo23@gmail.com>
2021-04-11 08:34:06 +0530

    mongo db cloud connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -17,10 +18,24 @@ app.get('/contact', function (req, res) {
   res.sendFile(__dirname+'/public/contact.html')
 })
 
+const password="YKTKDMgSsdQSIbfV"
+const dbUrl="mongodb+srv://web-user:"+password+"@ir-cluster.c9zhs.mongodb.net/ir?retryWrites=true&w=majority"
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const contactSchema={name:String, email:String, organization:String, designation:String, subject:String, message:String}
+const Contact = mongoose.model('Contact',contactSchema)
+
 app.post('/contact', function(req,res) {
   console.log(req.body)
+  const newContact = new Contact(req.body)
+  newContact.save()
+  res.redirect('/contact');
 })
 
+
+
+
+// Listening Port
 app.listen(3000,function() {
   console.log("Listening on port 3000")
 })
