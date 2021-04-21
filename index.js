0d8a86dc319e2cf54bc78909b3cf2ceddb76cd64
Sativa Wangnoo <sativawangnoo23@gmail.com>
2021-04-22 04:01:40 +0530

    Contact form submission

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ const password="YKTKDMgSsdQSIbfV"
 const dbUrl="mongodb+srv://web-user:"+password+"@ir-cluster.c9zhs.mongodb.net/ir?retryWrites=true&w=majority"
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const contactSchema={name:String, email:String, organization:String, designation:String, subject:String, message:String}
+const contactSchema={name:String, email:String, date:String, organization:String, designation:String, subject:String, message:String}
 const Contact = mongoose.model('Contact',contactSchema)
 
 app.post('/contact', function(req,res) {
-  console.log(req.body)
   const newContact = new Contact(req.body)
+  newContact.date = Date().toString()
   newContact.save()
-  res.redirect('/contact');
+  res.sendFile(__dirname+'/public/contactsaved.html');
 })
 
 
