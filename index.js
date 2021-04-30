50fe8fcc572afb5a66e067b8f1e5bfd99d06d378
Sativa Wangnoo <sativawangnoo23@gmail.com>
2021-04-30 12:57:23 +0530

    Contact.js created, contact completed

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
 const ejs = require('ejs');
 
 // Secrets  (Change at Last)
-const mongopassword="YKTKDMgSsdQSIbfV"
+
 
 // Listening Port
 const port = process.env.PORT || 3000
@@ -29,26 +28,10 @@ app.get('/:pagename',function(req,res) {
 })
 
 // Contact form
-app.get('/contact', function (req, res) {
-  console.log("Contact form Opened");
-  res.sendFile(__dirname+'/public/contact.html')
+const contact = require(__dirname+'/contact.js');
+app.post('/contact', function (req,res){
+  contact.submitContact(req,res)
 })
-
-const dbUrl="mongodb+srv://web-user:"+mongopassword+"@ir-cluster.c9zhs.mongodb.net/ir?retryWrites=true&w=majority"
-mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const contactSchema={name:String, email:String, date:String, organization:String, designation:String, subject:String, message:String}
-const Contact = mongoose.model('Contact',contactSchema)
-
-app.post('/contact', function(req,res) {
-  const newContact = new Contact(req.body)
-  newContact.date = Date().toString()
-  newContact.save()
-  res.sendFile(__dirname+'/public/contactsaved.html');
-})
-
-
-
 
 // Listening Port
 app.listen(port,function() {
